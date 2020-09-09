fa60b994e7bfc4f36bf6dcbee92245ea8ff3c883
Henry <sode787@yahoo.com>
2020-09-08 19:24:08 -0500

    finished up basic community resources page

--- src/components/CommunityResources.js
+++ src/components/CommunityResources.js
@@ -6,7 +6,7 @@
 /// summary
 
 /// To Do
-///   - 
+///   - make a map or use googles maps api for the className="map" section
 
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -15,6 +15,8 @@ import { Link } from 'react-router-dom';
 import '../css/main.css'; 
 import '../css/community_resources.css';
 
+// images
+import carrollton from '../pictures/carrollton.jpg';
 
 function CommunityResources() {
   return (
@@ -29,15 +31,27 @@ function CommunityResources() {
         {
           // later will add a map to the left and infomation to the right
         }
+
         <div>
+
           <div className="map">
-            <p>map</p>
+            <h3>Map</h3>
+            <img src={carrollton} width="80%"/> {/* not working*/}
           </div>
 
           <div className="info">
-            <p>information</p>
-          </div>
 
+            <div className="info-top">
+              <h3>Carrolton Health & Rehabilitation Center</h3>
+            </div>
+            
+            <div className="info-bottom">
+              <p>Phone Number: 972-245-1573</p>
+              <p>Address: 1618 Kirby Rd</p> {/* maybe make this a link to google maps in the future? */}
+              <p>Website: https://carrolltonhealth.com</p>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
