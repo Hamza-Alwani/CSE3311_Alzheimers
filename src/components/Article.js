66af35e38db375bb91b9aca17c5b6c38d1989a27
Henry <sode787@yahoo.com>
2020-09-15 23:42:36 -0500

    redo dementia information layout

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import styled from 'styled-components'
 
+//bootstrap 
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
 // css
 import '../css/main.css'; 
 import '../css/article.css'; 
@@ -13,22 +17,19 @@ function Article() {
 
     return (
         <ArticleContainer>
-            <div className="article-container">
-                
 
-                <div className="pic">
-                    <img src={pic1} alt="pic of lady"/> 
-                </div>
 
-                <div className="title">
-                    <a href="https://google.com" target="_blank"  rel="noopener noreferrer">  learn about alzimers </a>
-                </div>
-
-                <div className="discription">
-                    <p> this is a discription for the thing asdasd</p>
-                </div>
-
-            </div>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={pic1} />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
          </ArticleContainer>
     );
 }
@@ -38,91 +39,5 @@ export default Article;
 
 const ArticleContainer = styled.nav`
 
-    .article-container
-    {
-        overflow: hidden;
-        position: relative;
-        width: 100%;
-    }
-
-    .article-container ul 
-    {	
-        /*top: 100%;*/
-        width: 100%;
-    }
-
-    .article-container ul li
-    {
-        float: left;
-        width: 30%;
-        list-style: none;
-    }
-
-    .article-container ul li img
-    {
-        float: left;
-        width: 100%;
-        list-style: none;
-        margin: 0px;
-    }
-
-    .article
-    {
-        text-align: center;
-    }
-
-    ${'' /* .jBDKcv .container 
-    {
-        overflow: hidden;
-        position: relative;
-        width: 100%;    
-    } */}
-
-    .article-container 
-    {
-        max-height: 400px;
-        max-width: 400px;
-        width: auto;
-        height: auto;
-        margin: .5% .5%;
-        padding: 1%;
-        display: inline-block;
-        background-color: gray;
-        text-align: center;
-    }
-
-    .pic img
-    {
-    max-width: auto;
-    max-height: auto;
-    width: 100%;
-    height: 100%;
-    margin-bottom: 2% ;
-    }   
-
-    .title a {
-        max-width: 500px;
-        max-height: 50px;
-        width: 100%;
-        height: 100%;
-        margin: 10% 0%;   
-        text-align: left;
-        font-size: x-large;
-        width: 100%;
-        padding-top: 1%;
-        color: black;
-        text-decoration:underline ;
-        
-
-
-    }
-
-    .discription p{
-        max-height: fit-content;
-        padding-top: 2%;
-        width: auto;
-        height: auto;
-        font-size:auto ;
-    }
 
 `;
