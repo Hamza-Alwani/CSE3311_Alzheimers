de9134de1261b40a02638213eb34fb3b30b05411
Hamza.A <hamzaalwani@gmail.com>
2020-09-16 15:44:48 -0500

    admin email viewer done

--- src/components/EmailViewer.js
+++ src/components/EmailViewer.js
@@ -6,70 +6,46 @@ import React from 'react';
 // components
 import { Button } from 'react-bootstrap';
 import  firebase from '../components/firebase'
-
 //css
 import '../css/email_viewer.css'
 
 
-/*
-var keys=[];
-var i=0;  
-const ref=firebase.database().ref("/contact_us")
-ref.once('value', function(snapshot) {
-   snapshot.forEach(function(childSnapshot) {
-      var childKey = childSnapshot.key;
-      return keys[i]=childKey;
-      i++
-   });
-   return keys;
- });
-
- const keyslist=keys.map((element) =>
- <li>{element}</li>
- );
-
- 
-firebase.database().ref("/contact_us").on('value', (snapshot)=>{
-   snapshot.forEach((childsnapshot)=>{
-      var childKey = snapshot.key;
-      let keys = Object.keys(childKey);
-      keys.forEach((key) => {  
-      window.alert(keys.valueOf(key));
-      });
-   });
- //  var childKey = snapshot.key;
-  // window.alert(childKey);
-  // let keys = Object.keys(childKey);
-  // keys.forEach((key) => {  });
-});
-*/
 
 
-function EmailVeiwer(index,name,email,body) 
+
+function EmailVeiwer(obj) 
 {
-   // console.log("EmailVietwe");
-   
 
    return(    
          <div className="boundry">
              <div className="body">
                 {
-                  
-                   
-                                    
+                   <p>
+                      name={obj.name}
+                      <br></br>
+                      email={obj.emailaddress}
+                      <br></br>
+                      emailbody={obj.emailaddress}
+                   </p>                       
                 }
              </div>
-             <Button> Delete </Button>
+             <Button onClick={() => delete_button_pressed(obj.id)}>Delete</Button>
          </div>
       );
-
 }
 
+export default EmailVeiwer 
 
-    
-        
-    
 
- 
+function delete_button_pressed(obj){
 
-export default EmailVeiwer /*	Change this part to match the class name above */
+   firebase.database().ref('contact_us').child(obj).remove().then(function(){
+      window.alert("Message Deleted");
+      window.location.reload(false);
+   }).catch(function(error){
+   });
+}
+
+  
+      
+  
--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -12,83 +12,15 @@ import EmailViewer from '../components/EmailViewer';
 import  firebase from '../components/firebase';
 //css
 import '../css/main.css'
-import { Button } from 'react-bootstrap';
 import '../css/email_viewer.css'
 
 
 
 
 
-/*
- 
-firebase.database().ref("/contact_us").on('value', (snapshot)=>{
-   snapshot.forEach((childsnapshot)=>{
-      var childKey = snapshot.key;
-      let keys = Object.keys(childKey);
-      keys.forEach((key) => {  
-      window.alert(keys.valueOf(key));
-      });
-   });
- //  var childKey = snapshot.key;
-  // window.alert(childKey);
-  // let keys = Object.keys(childKey);
-  // keys.forEach((key) => {  });
-});
-*/
-
-
-
-
-// function AdminPage (){ 
-//    var keys = [   ];
-
-//    var i=0;
-
-
-   
-//    firebase
-//       .database()
-//       .ref("/contact_us")
-//       .on('value', (snapshot)=>{
-//          snapshot.forEach((childsnapshot)=>{
-//             var childKey = childsnapshot.key;
-//             keys[i]=childKey;
-//             i++
-//          // window.alert(keys);
-//          });
-//       });
-
-//       console.log(keys);
-      
-      
-//    return(    
-//          <div className="all-content">
-//             <Nav></Nav> 
-//             <div>
-//             </div>
-//             <div>
-
-//                {  
-//                   <EmailViewer 
-//                   name={keys}
-//                   />
-                  
-//                }
-               
-//             </div>
-//             <Footer></Footer>
-//          </div>
-//       );
-//    }
-
-   
-// export default  AdminPage
-
-
-// operates the same way as a function, but you can manipulate data with other functions and use lifecycle methods
 class AdminPage extends Component 
 {
-   // construct and object to save data
+
    constructor()
    {
       super()
@@ -101,7 +33,7 @@ class AdminPage extends Component
       }
    }
 
-   // life cycle method coming from the Component library
+
    componentDidMount()
    {
       const listref=firebase.database().ref().child('contact_us').orderByKey();
@@ -125,7 +57,7 @@ class AdminPage extends Component
 
    render()
    {
-      // EmailViewer takes all the emails, i was just too lazy to list them all
+     
       
       return (
          <div>
@@ -134,28 +66,16 @@ class AdminPage extends Component
            <body>
                  {
               this.state.ref.map((dataList,index)=>
-              <div className="boundry">
-                <div className="body">
-                     <p>  
-                        name={this.state.names[index]}
-                        <br></br>
-                        emailaddress={this.state.emails[index]}
-                        <br></br>
-                        emailbody={this.state.emailbodys[index]}
-                        <br></br>
-                        <Button > Delete </Button>
-                     </p>
-                  </div> 
-                </div>
+              <EmailViewer 
+              id= {this.state.ref[index]}
+              name={this.state.names[index]} 
+              emailaddress={this.state.emails[index]}
+              emailbody={this.state.emailbodys[index]}
+              />
               )
            } 
-                 
-
-              
-           </body>
-          
-              
-            <Footer/>
+            </body>       
+         <Footer/>
          </div>
       )
    }
