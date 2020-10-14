e042caf26d9556c821fa09f50689940470e2e8f4
Henry <sode787@yahoo.com>
2020-10-14 14:04:48 -0500

    add lang buttons to community, broken (add, update delete) for community

--- src/admin_pages/AdminCommunityResources/AdminCommunityComponents/AdminAddCommunity.js
+++ src/admin_pages/AdminCommunityResources/AdminCommunityComponents/AdminAddCommunity.js
@@ -99,14 +99,14 @@ function AdminAddCommunity() {
          <StyleCommunityContainer>
 
             {/* Drop down to pick citys */}
-            <Form onSubmit={add_button_pressed} className="contact-us-form">
+            <Form className="contact-us-form">
                   <Form.Group>
                      <Form.Label>Name</Form.Label>
-                     <Form.Control type="name" id="name" placeholder="name" />
+                     <Form.Control type="name" id="name"  />
                   </Form.Group>
                   <Form.Group>
                      <Form.Label>Address</Form.Label>
-                     <Form.Control type="name" id="address" placeholder="address" />
+                     <Form.Control type="name" id="address"  />
                   </Form.Group>
                   <Form.Group>
                      <Form.Label>State</Form.Label>
@@ -122,21 +122,21 @@ function AdminAddCommunity() {
                   </Form.Group>
                   <Form.Group>
                      <Form.Label>City</Form.Label>
-                     <Form.Control type="name" id="city"  placeholder="city"/>
+                     <Form.Control type="name" id="city"  />
                   </Form.Group>
                   <Form.Group>
                      <Form.Label>Google Map</Form.Label>
-                     <Form.Control type="name" id="googleMap" placeholder="<iframe>Long Link</iframe>"/>
+                     <Form.Control type="name" id="googleMap" />
                   </Form.Group>
                   <Form.Group>
                      <Form.Label>Phone</Form.Label>
-                     <Form.Control type="name" id="phone" placeholder="phone"/>
+                     <Form.Control type="name" id="phone" />
                   </Form.Group>
                   <Form.Group>
                      <Form.Label>Website</Form.Label>
-                     <Form.Control type="name" id="website" placeholder="web url"/>
+                     <Form.Control type="name" id="website" />
                   </Form.Group>
-                  <Button variant="primary" type="submit" className="submit">Add</Button>
+                  <Button onClick={() => add_button_pressed()} variant="primary" type="" className="submit">Add</Button>
                </Form>
          </StyleCommunityContainer>
       </div>
