dc636e73fb3766e29b0536875065f3edbca5d0d2
Henry <sode787@yahoo.com>
2020-11-07 17:38:15 -0600

    dementia page - implemented horizontal cards - not mobile compatible

--- src/components_ch/Article_ch.js
+++ src/components_ch/Article_ch.js
@@ -1,34 +1,86 @@
 /// summary
-///   Used to display Articles on Dementia Information page
+///
+///	Article_xx  
+/// - A template that will be useed by DisplayArticle_XX to create card like information
+///
 /// summary
 
+
+
 import React from 'react';
-
 import styled from 'styled-components'
-
-//bootstrap 
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-
-// css
-
 import '../css/main.css'; 
 
 function Article( {props} ) {
-
     return (
-
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={props.pic} />
-                <Card.Body>
-                    <Card.Title> {props.title} </Card.Title>
-                    <Card.Text> {props.disc} </Card.Text>
-                    <a href={props.website} target="_blank"> <Button variant="primary">Go </Button></a>
-                </Card.Body>
-            </Card>
+            <ArticleContainer>
+                <a className="article-text-container"  href={props.website}>
+                    <img className="flex-child" style={{ width:"300px", height:"200px" }} src={props.pic} ></img>
+                    <div className="flex-child">
+                        <div className="article-text-info">
+                            <h3>{props.title}</h3>
+                            <p>{props.disc}</p>
+                        </div>
+                    </div>
+                </a>
+            </ArticleContainer>
     );
 }
 
 
 export default Article;
 
+const ArticleContainer = styled.div`
+
+.article-text-container {
+    text-decoration: none;
+    color: black;
+    display: flex;
+    margin-left: 5rem;
+    margin-bottom: 2rem;
+
+    border-radius: 5px;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    -webkit-transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+    transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+}
+
+.article-text-container::after {
+    content: "";
+    border-radius: 5px;
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+    opacity: 0;
+    -webkit-transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+    transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+}
+
+.article-text-container:hover {
+    -webkit-transform: scale(1.25, 1.25);
+    transform: scale(1.25, 1.25);
+}
+
+.article-text-container:hover::after {
+    opacity: 1;
+}
+
+.flex-child
+{
+    ${'' /* border: 2px solid yellow; */}
+    height: 200px;
+    width: 80%;
+}
+
+.article-text-info
+{   
+    margin-top:1rem;
+    margin-left: 2rem;
+}
+
+`;
